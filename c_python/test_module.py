74d49ffb3ab6067064b53bb37a59471236c61c82
ziv <north780316@gamil.com>
2023-12-16 12:40:17 +0800

    [Description] update
    [Commit Files]
    c_python/test_module.py
    
    -------------------------------------------

--- c_python/test_module.py
+++ c_python/test_module.py
@@ -1,6 +1,14 @@
 import c_performance as c
 import numpy as np
 
+print( c.__dir__() )
+print( 'c.name=', c.__name__ )
+print( 'c.file=', c.__file__ )
+print( 'c.loader=', c.__loader__ )
+print( 'c.package=', c.__package__ )
+print( 'c.doc=', c.__doc__ )
+print( 'c.spec=', c.__spec__ )
+
 val = c.norm( order=2, vector=[1, 2, 3], debug=1 );
 print( 'c.norm2 =', val );
 
